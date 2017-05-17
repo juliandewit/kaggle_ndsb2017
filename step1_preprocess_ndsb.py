91b64ce0a129d3ec37d93fc832a25f1a6c98d1c8
xia <604135528@qq.com>
2017-05-17 17:13:14 +0800

    adjust image generated from dicom
    if there exists slope in dicom,rotation image with corresponding degree

--- step1_preprocess_ndsb.py
+++ step1_preprocess_ndsb.py
@@ -48,6 +48,10 @@ def resample(image, scan, new_spacing=[1, 1, 1]):
     image = scipy.ndimage.interpolation.zoom(image, real_resize_factor)
     return image, new_spacing
 
+def cv_flip(img,cols,rows,degree):
+    M = cv2.getRotationMatrix2D((cols / 2, rows /2), degree, 1.0)
+    dst = cv2.warpAffine(img, M, (cols, rows))
+    return dst
 
 def extract_dicom_images_patient(src_dir):
     target_dir = settings.NDSB3_EXTRACTED_IMAGE_DIR
@@ -57,7 +61,10 @@ def extract_dicom_images_patient(src_dir):
     slices = load_patient(dir_path)
     print(len(slices), "\t", slices[0].SliceThickness, "\t", slices[0].PixelSpacing)
     print("Orientation: ", slices[0].ImageOrientationPatient)
-    assert slices[0].ImageOrientationPatient == [1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000]
+    #assert slices[0].ImageOrientationPatient == [1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000]
+    cos_value = (slices[0].ImageOrientationPatient[0])
+    cos_degree = round(math.degrees(math.acos(cos_value)),2)
+    
     pixels = get_pixels_hu(slices)
     image = pixels
     print(image.shape)
@@ -77,6 +84,9 @@ def extract_dicom_images_patient(src_dir):
             os.mkdir(patient_dir)
         img_path = patient_dir + "img_" + str(i).rjust(4, '0') + "_i.png"
         org_img = image[i]
+        # if there exists slope,rotation image with corresponding degree
+        if cos_degree>0.0:
+            org_img = cv_flip(org_img,org_img.shape[1],org_img.shape[0],cos_degree)
         img, mask = helpers.get_segmented_lungs(org_img.copy())
         org_img = helpers.normalize_hu(org_img)
         cv2.imwrite(img_path, org_img * 255)
